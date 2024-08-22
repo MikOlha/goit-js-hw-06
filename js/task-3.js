2bb83c342947caaa761a3e1fd9e25809ace8065a
Olha Mikushynska <168028895+MikOlha@users.noreply.github.com>
2024-08-22 17:37:26 +0300

    correction

--- js/task-3.js
+++ js/task-3.js
@@ -1,22 +1,23 @@
 class StringBuilder {
+  #value;
   constructor(initialValue) {
-    this.value = initialValue;
+    this.#value = initialValue;
   }
 
   getValue() {
-    return this.value;
+    return this.#value;
   }
 
   padEnd(str) {
-    this.value += str;
+    this.#value += str; // Додає str в кінець value
   }
 
-  padStart(str) {
-    this.value = str + this.value;
+    padStart(str) {
+    this.#value = str + this.#value;
   }
 
-  padBoth(str) {
-    this.value = str + this.value + str;
+ padBoth(str) {
+   this.#value = str + this.#value + str;
   }
 }
 
